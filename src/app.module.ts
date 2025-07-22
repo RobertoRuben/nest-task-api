297f49dd5f9290ee9c5243bf0800ddcc77a42866
RobertoRuben <robertoch263@gmail.com>
2025-07-22 12:55:24 -0500

    ✨ feat(app): configure Mongoose and ConfigModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        uri: `mongodb://${config.get('MONGODB_USER')}:${config.get('MONGODB_PASSWORD')}@${config.get('MONGOBD_HOST')}:${config.get('MONGODB_PORT')}/${config.get('MONGODB_DATABASE')}`,
+      }),
+      inject: [ConfigService],
+    }),
+    TaskModule,
+  ],
   controllers: [],
   providers: [],
 })
